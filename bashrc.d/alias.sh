f01cf24c94fd35d9b8230baf1d3cc20edc131c72
Colin Henry <jchenry@users.noreply.github.com>
2021-09-05 02:03:48 -0700

    some cleanup

--- bashrc.d/alias.sh
+++
@@ -1,3 +0,0 @@
-alias build-targets="make -rpn | sed -n -e '/^$/ { n ; /^[^ .#][^ ]*:/p ; }' | egrep --color '^[^ ]*:'"
-alias ebg="tr 'A-Za-z' 'N-ZA-Mn-za-m'"
-alias tmux="tmux -f "$XDG_CONFIG_HOME"/tmux/tmux.conf"
