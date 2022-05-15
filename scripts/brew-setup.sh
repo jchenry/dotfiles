b353f35ec506516fde80be181e837eeee1b6f71a
Colin Henry <jchenry@users.noreply.github.com>
2022-05-15 12:09:09 -0700

    fix brewfile location

--- scripts/brew-setup.sh
+++ scripts/brew-setup.sh
@@ -2,4 +2,4 @@
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew tap Homebrew/bundle
 mas signin colin@jchenry.me
-brew bundle --file ../Brewfile
+brew bundle --file ~/.config/Brewfile
