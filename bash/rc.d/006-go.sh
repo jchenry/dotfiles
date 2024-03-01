90969363bc2c76ee099615c502376c5cdaf320cc
Colin Henry <jchenry@users.noreply.github.com>
2024-02-29 16:48:30 -0800

    updates to bash config

--- bash/rc.d/006-go.sh
+++ bash/rc.d/006-go.sh
@@ -3,5 +3,5 @@ export GOPATH=$XDG_DATA
 export GOROOT=/usr/local/go
 export GOBIN=$XDG_DATA/bin
 export GO111MODULE=on
-export GOPRIVATE=github.com/jchenry 
+export GOPRIVATE=github.com/jchenry/*
 export PATH=$PATH:$GOROOT/bin:$GOBIN
---
+++ bash/rc.d/011-java.sh
@@ -0,0 +1,2 @@
+export JAVA_HOME=$HOME/Library/Java/JavaVirtualMachines/openjdk-18.0.2.1/Contents/Home
+export PATH=$PATH:$JAVA_HOME/bin
